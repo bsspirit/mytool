5ffe4560ef19cd5eee1c2d9d3a12505ec3dbcb26
Conan Zhang <bsspirit@163.com>
2011-10-19 14:53:11 +0800

    'a'

--- md5-dict.py
+++ md5-dict.py
@@ -8,8 +8,26 @@ def num6():
 	for i in range(100000,999999):
 		print str(i) + ',' + md5(i)
 
+#Èõ¿ÚÁî
+def weak_word():
+	f = open('dict/weakword.txt',"r")
+	line = f.readline()
+	while line:
+		i = line[:-1]
+		print str(i) + ',' + md5(i)
+		line=f.readline()
+	f.close()
+
+	
+	#for i in a:
+	#	print str(i) + ',' + md5(i)
+
 def md5(arg):
 	return str(hashlib.md5(str(arg)).hexdigest())
+	
+
+	
 
 if __name__=="__main__":
-	num6()
+	#num6()
+	weak_word()
